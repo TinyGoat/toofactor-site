4d8055a8bf77c85c4167813a93ff49773a0985f2
littlerobothead <nick@littlerobothead.com>
2012-04-26 22:10:14 -0400

    All the things.

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'pg'
+gem 'jquery-rails'
+
+group :development do
+  gem 'unicorn'
+  gem 'pry'
+  gem 'awesome_print'
+  gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'compass-rails'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toofactor::Application.config.secret_token = 'c3424ce71b91bd23492155f43ad1687159284251e278d9d6f87faaf23289eb234669a65f40728822a47c7c3bb227b1dbdcce4c809f0725bb0e3a8ae0d90ef73a'
