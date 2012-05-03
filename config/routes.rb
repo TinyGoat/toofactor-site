309b341684fdb3a290ef7aea7e8c01b4a2348bf5
littlerobothead <nick@littlerobothead.com>
2012-05-03 14:35:47 -0400

    Front end bits and pieces.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Toofactor::Application.routes.draw do
+  get "contact/index"
+
   devise_for :users
 
   get "plans", to: "plans#index"
+  get "contact", to: "contact#index"
 
   root :to => 'home#index'
 end
