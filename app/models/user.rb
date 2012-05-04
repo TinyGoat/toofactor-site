11177ac90dc9db6c4ef7d619200367d5043e8f69
Geoff Parsons <geoffrey.parsons@gmail.com>
2012-05-03 22:11:26 -0400

    Setting next billing date (currently 15 days in the future) when users are created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,33 @@ class User < ActiveRecord::Base
                   :password_confirmation,
                   :remember_me,
                   :stripe_token,
-                  :api_key
+                  :api_key,
+                  :next_billing_date
+
+
+  ##
+  ## Validations
+  ##
+
+  validates :email, presence: true, uniqueness: true
+  validates :next_billing_date, presence: true
+  validates :api_key, presence: true, uniqueness: true
 
   ##
   ## Callbacks
   ##
 
-  before_create :generate_api_key
+  before_validation :generate_api_key, on: :create
+  before_validation :set_next_billing_date, on: :create
 
 
   private
   def generate_api_key
     self.api_key = (Digest::RMD160.new << self.email + API_KEY_SALT).to_s
+  end
+
+  def set_next_billing_date
+    self.next_billing_date = 15.days.from_now
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120504012629) do
+ActiveRecord::Schema.define(:version => 20120504020613) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120504012629) do
     t.datetime "updated_at",                             :null => false
     t.string   "stripe_token"
     t.string   "api_key"
+    t.date     "next_billing_date"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
