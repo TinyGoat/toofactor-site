fb5ba48afcbdff3afc235386c5121bf82ba96759
Geoff Parsons <geoffrey.parsons@gmail.com>
2012-05-11 21:23:58 -0400

    Requiring HTTP auth on staging; username and password are set via the HTTP_AUTH_USER_NAME and HTTP_AUTH_PASSWORD environment variables.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :require_http_auth_on_staging
+
+
+  private
+  def require_http_auth_on_staging
+    if Rails.env.staging?
+      authenticate_or_request_with_http_basic do |user_name, password|
+        if ENV['HTTP_AUTH_USER_NAME'].blank? || ENV['HTTP_AUTH_PASSWORD'].blank?
+          true
+        else
+          user_name == ENV['HTTP_AUTH_USER_NAME'] && password == ENV['HTTP_AUTH_PASSWORD']
+        end
+      end
+    end
+  end
 end
