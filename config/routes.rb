5f065835aa4cdc29f20882b072720c2fc6b9f468
Geoff Parsons <geoffrey.parsons@gmail.com>
2012-05-12 21:41:52 -0400

    Cleaned up contact form and removed unused routes in the landing version of the app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Toofactor::Application.routes.draw do
-
   get '/contact' => 'contact#index', as: :contact_us
   post '/contact' => 'contact#contact', as: :make_contact
-
-  devise_for :users
-
-  get "plans", to: "plans#index"
-  get "contact", to: "contact#index"
-
   root :to => 'home#index'
 end
